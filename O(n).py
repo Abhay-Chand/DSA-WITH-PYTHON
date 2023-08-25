3db6c0743506e481232bf5171c184e21f873aae7
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2023-08-25 12:42:30 +0530

    Add files via upload
    this is based on O(n)or linear runtime complexity

---
+++ O(n).py
@@ -0,0 +1,7 @@
+#O(n) runtime complexity is also called linear
+def print_number(n):
+    for i in range(n):
+        print(i)
+print_number(20)
+#this code is for linear runtime complexity
+#and if we talk about graph it is continous in straight line in the respect of element vs opration .where element is in base
