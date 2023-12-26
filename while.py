37311cc8e6a52d3ca42e9b1617f71ae326cf3ef2
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2023-12-26 13:15:07 +0530

    1.#W.A.P to print 1 to N.

---
+++ while.py
@@ -0,0 +1,6 @@
+#W.A.P to print 1 to N.
+N = int(input("Enter how many time u have to print :"))
+i=0 
+while(i<N):
+        print("hyy dude")
+        i=i+1
---
+++ while2.py
@@ -0,0 +1,5 @@
+# W.A.P to print reverse the natural number 
+N =int(input("Enter no. of natural number you have to print reverse:"))
+while (N>0):
+    print(N)
+    N = N-1
