3a7759211f107494d100807b2ef217ca9a0a5861
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2023-12-27 06:43:13 +0530

    program for sum of square of natural number

---
+++ while4.py
@@ -0,0 +1,9 @@
+# W.A.P TO PRINT SUM sauare OF N NATURAL NUMBER
+N = int(input("Enter sum of natural number you want : "))
+i = 0
+sum = 0
+while (i<=N):
+    
+    sum = sum +(i**2)
+    i = i+1
+print("sum :",sum)    
