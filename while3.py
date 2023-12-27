4d539c43b2b77f646c3f0fdd98162206f07fbd74
OKA <113808591+Abhay-Chand@users.noreply.github.com>
2023-12-27 06:35:21 +0530

    FOR SUM OF N NATURAL NUMBER

---
+++ while3.py
@@ -0,0 +1,8 @@
+# W.A.P TO PRINT SUM OF N NATURAL NUMBER
+N = int(input("Enter sum of natural number you want : "))
+i = 0
+sum = 0
+while (i<N):
+    sum = sum +i
+    i = i+1
+print("sum :",sum)    
